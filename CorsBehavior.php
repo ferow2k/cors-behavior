b525c2c976837c3682886d11a5d7ba2c6c947d9a
Ahmad Asjad <ahmadcimage@gmail.com>
2019-02-25 12:31:40 +0530

    getAllHeaders method not worked on nginx server

--- CorsBehavior.php
+++ CorsBehavior.php
@@ -2,7 +2,7 @@
 
 /*
  * CorsBehavior class file
- * 
+ *
  * @author Igor Manturov, Jr. <im@youtu.me>
  * @link https://github.com/iAchilles/cors-behavior
  */
@@ -10,52 +10,58 @@
 /**
  * CorsBehavior Automatically adds the Access-Control-Allow-Origin response 
  * header for specific routes.
- * 
+ *
  * @version 1.0
  *
  */
 class CorsBehavior extends CBehavior
 {
-    
+
     private $_allowOrigin;
     public $allowMethods = false;
     public $allowHeaders = false;
-    
+
     private $_route = array();
-    
-    
+
+    private $cors = [
+        'Origin' => ['*'],
+        'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+        'Access-Control-Request-Headers' => ['*'],
+        'Access-Control-Allow-Credentials' => null,
+        'Access-Control-Max-Age' => 86400,
+        'Access-Control-Expose-Headers' => [],
+    ];
+
     public function events()
     {
-        return array_merge(parent::events(), 
+        return array_merge(parent::events(),
                 array('onBeginRequest' => 'onBeginRequestHandler'));
     }
-    
-    
+
     public function onBeginRequestHandler($event)
     {
         if (is_null($this->_allowOrigin))
         {
             return;
         }
-        
+
         if ($this->checkAllowedRoute())
         {
-            
+
             $origin = $this->parseHeaders();
-            
+
             if ($origin !== false)
             {
                 $this->setAllowOriginHeader($origin);
             }
         }
     }
-    
-    
+
     /**
      * Sets list of routes for CORS-requests.
-     * @param mixed $route An array of routes (controllerID/actionID). If you 
+     * @param mixed $route An array of routes (controllerID/actionID). If you
      * want to allow CORS-requests for any routes, the value of the parameter
-     * must be a string that contains the "*". 
+     * must be a string that contains the "*".
      * @throws CException
      */
     public function setRoute($route)
@@ -65,17 +71,16 @@ class CorsBehavior extends CBehavior
             throw new CException('The value of the "route" property must be an '
                     . 'array or a string that contains the "*".');
         }
-        
+
         $this->_route = $route;
     }
-    
-    
+
     /**
      * Sets the allowed origin.
      * @param string $origin The origin that is allowed to access the resource.
-     * A "*" can be specified to enable access to resource from any origin. 
-     * A wildcard can be used to specify list of allowed origins, 
-     * e.g. "*.yourdomain.com" (sub.yourdomain.com, yourdomain.com, 
+     * A "*" can be specified to enable access to resource from any origin.
+     * A wildcard can be used to specify list of allowed origins,
+     * e.g. "*.yourdomain.com" (sub.yourdomain.com, yourdomain.com,
      * sub.sub.yourdomain.com will be allowed origins in that case)
      * @throws CExecption
      */
@@ -86,47 +91,39 @@ class CorsBehavior extends CBehavior
             throw new CExecption('The value of the "allowOrigin" property must be '
                     . 'a string.');
         }
-        
+
         $this->_allowOrigin = $origin;
     }
-    
-    
+
     /**
      * Parses headers and returns the value of the Origin request header.
-     * @return mixed The origin that is allowed to access the resource. 
-     * (the value of the Origin request header), otherwise false. 
+     * @return mixed The origin that is allowed to access the resource.
+     * (the value of the Origin request header), otherwise false.
      */
     protected function parseHeaders()
     {
-        if (!function_exists('getallheaders'))
-        {
-            $headers = $this->getAllHeaders();
-        }
-        else
-        {
-            $headers = getallheaders();
-        }
-        
+        $headers = $this->extractHeaders();
+
         if ($headers === false)
         {
             return false;
         }
-        
+
         $headers = array_change_key_case($headers, CASE_LOWER);
-        
+
         if (!array_key_exists('origin', $headers))
         {
             return false;
         }
-        
+
         $origin = $headers['origin'];
         $origin = parse_url($origin, PHP_URL_HOST);
-        
+
         if (is_null($origin))
         {
             return false;
         }
-        
+
         if(strlen($this->_allowOrigin) === 1)
         {
             return $headers['origin'];
@@ -150,8 +147,7 @@ class CorsBehavior extends CBehavior
         }
 
     }
-    
-    
+
     /**
      * Checks if CORS-request is allowed for the current route.
      * @return boolean Whether CORS-request is allowed for the current route.
@@ -162,7 +158,7 @@ class CorsBehavior extends CBehavior
         {
             return true;
         }
-        
+
         $route = Yii::app()->getUrlManager()
                 ->parseUrl(Yii::app()->getRequest());
 
@@ -189,11 +185,10 @@ class CorsBehavior extends CBehavior
         }
         return false;
     }
-    
-    
+
     /**
      * Sets Access-Control-Allow-Origin response header.
-     * @param string $origin the value of the Access-Control-Allow-Origin response 
+     * @param string $origin the value of the Access-Control-Allow-Origin response
      * header.
      */
     protected function setAllowOriginHeader($origin)
@@ -211,26 +206,36 @@ class CorsBehavior extends CBehavior
             \Yii::app()->end();
         }
     }
-    
-    
+
     /**
-     * This method is used to get HTTP headers when PHP runs as FastCGI.
-     * @return array An associative array of all the HTTP headers in the current request.
+     * Extract CORS headers from the request.
+     * @return array CORS headers to handle
      */
-    protected function getAllHeaders()
+    public function extractHeaders()
     {
-       $headers = ''; 
-       
-       foreach ($_SERVER as $name => $value) 
-       { 
-           if (substr($name, 0, 5) == 'HTTP_') 
-           { 
-               $headers[str_replace(' ', '-', ucwords(strtolower
-                       (str_replace('_', ' ', substr($name, 5)))))] = $value; 
-           } 
-       } 
-       
-       return $headers; 
+        $headers = [];
+        foreach (array_keys($this->cors) as $headerField) {
+            $serverField = $this->headerizeToPhp($headerField);
+            $headerData = isset($_SERVER[$serverField]) ? $_SERVER[$serverField] : null;
+            if ($headerData !== null) {
+                $headers[$headerField] = $headerData;
+            }
+        }
+
+        return $headers;
     }
-   
+
+    /**
+     * Convert any string (including php headers with HTTP prefix) to header format.
+     *
+     * Example:
+     *  - X-Pingother -> HTTP_X_PINGOTHER
+     *  - X PINGOTHER -> HTTP_X_PINGOTHER
+     * @param string $string string to convert
+     * @return string the result in "php $_SERVER header" format
+     */
+    protected function headerizeToPhp($string)
+    {
+        return 'HTTP_' . strtoupper(str_replace([' ', '-'], ['_', '_'], $string));
+    }
 }
